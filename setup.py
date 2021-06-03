f2a4616f8e182493cd230482ab00f1bd8df132b2
duraiganesh0 <ganeshkumark@cystaldelta.com>
2021-06-04 01:30:54 +0530

    ironwood changes (#1)
    * ironwood changes
    
    * Update setup.py
    
    * Update utils.py
    
    * Update setup.py
    
    * Update setup.py

--- setup.py
+++ setup.py
@@ -53,10 +53,9 @@ setup(
         'git+https://github.com/edx/xblock-utils.git@v1.0.5#egg=xblock-utils==1.0.5',
     ],
     install_requires=[
-        'XBlock>=0.4.10,<2.0.0',
-        'pycaption>=0.7.1,<1.0',  # The latest Python 2.7 compatible version
         'requests>=2.9.1,<3.0.0',
         'babelfish>=0.5.5,<0.6.0',
+        'XBlock>=0.4.10,<2.0.0',
     ],
     entry_points={
         'xblock.v1': [
