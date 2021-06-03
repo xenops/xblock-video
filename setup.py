9b021df2de16ecac316faa488b09e6a526e02b53
duraiganesh0 <ganeshkumark@cystaldelta.com>
2021-06-03 12:10:37 +0530

    Removed dependencies for ironwood support

--- setup.py
+++ setup.py
@@ -54,8 +54,6 @@ setup(
     ],
     install_requires=[
         'XBlock>=0.4.10,<2.0.0',
-        'xblock-utils>=1.0.2,<=1.0.5',
-        'pycaption>=0.7.1,<1.0',  # The latest Python 2.7 compatible version
         'requests>=2.9.1,<3.0.0',
         'babelfish>=0.5.5,<0.6.0',
     ],
