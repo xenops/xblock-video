731d5e46ff7a102c31b42cdacb802d97fb76db55
duraiganesh0 <ganeshkumark@cystaldelta.com>
2021-06-04 20:06:41 +0530

    added pycaption back

--- setup.py
+++ setup.py
@@ -53,6 +53,7 @@ setup(
         'git+https://github.com/edx/xblock-utils.git@v1.0.5#egg=xblock-utils==1.0.5',
     ],
     install_requires=[
+        'pycaption==0.7.7',
         'requests>=2.9.1,<3.0.0',
         'babelfish>=0.5.5,<0.6.0',
         'XBlock>=0.4.10,<2.0.0',
